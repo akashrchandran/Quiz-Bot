4c9978f88850aead65684580ea8704df448f1f9a
Akash R Chandran <chandranrakash@gmail.com>
2024-02-05 16:13:18 +0000

    fixed params

--- quizbot/modules/downloader.py
+++ quizbot/modules/downloader.py
@@ -2,7 +2,7 @@ import gdown
 import os
 
 class FilesDownloder:
-    def __init__(self, url: str, file_name: str, file_path: str):
+    def __init__(self):
         self.qa_model_name = "multitask-qg-ag.ckpt"
         self.qa_model_id = "1ZhLe_nO_1VxIoVj5zRB61Ql-5_l0ZTJi"
         self.qa_model_path = "quizbot/ml_models/question_generation/models/multitask-qg-ag.ckpt"
